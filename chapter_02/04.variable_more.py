97ed0ee4e347c6669b237b0a437fad4b4c538e31
TechMainul <nilakashkhan2@gmail.com>
2020-10-21 04:29:45 +0600

    python 04.variable_more.py added

---
+++ chapter_02/04.variable_more.py
@@ -0,0 +1,14 @@
+# User input
+# Input Function
+
+print("\n question_01.............>")
+name, age = " Akash. ", "26"
+print("hello" + name + "your age is " + age)
+
+print()
+x = y = z = 1
+print("total = " + str(x + y + z))
+
+
+print()
+print("Thanks for your AREPL.......")
