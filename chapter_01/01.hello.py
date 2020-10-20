19e2771f2ef6e4f7fc290f7ca9b71bb1cb8e3441
TechMainul <nilakashkhan2@gmail.com>
2020-10-21 02:32:46 +0600

    folder name changes only

---
+++ chapter_01/01.hello.py
@@ -0,0 +1,8 @@
+print ('\n 1\' for single quote: \t\\\'')
+print ('\n 2\" for double quote: \t\\\"')
+
+print ('\n 3\\ for backslash: \t\\\\')
+print ('\n 4.\n\t for new line: \t\\n')
+print ('\n 5.\t for tab: \t\\t')
+print ('\n 6.\b for backspace: \t\\b')
+print()
