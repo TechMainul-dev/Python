67c269183d54a01d28860f0d6a31b1b760407292
TechMainul <nilakashkhan2@gmail.com>
2020-10-21 03:01:34 +0600

    python 01.strings.py added only

---
+++ chapter_02/01.strings.py
@@ -0,0 +1,12 @@
+# strings
+# collections of characters inside single quotes or double quotes
+print()
+first_name = "Akash"
+last_name = " Khan"
+
+full_name = first_name + last_name
+
+print(full_name)
+print(full_name + " 5")
+print(full_name + str(" 4"))
+print(full_name * 3)
