d382d1af5f7589f9d7b2457c3b0ee11c2c8e3418
TechMainul <nilakashkhan2@gmail.com>
2020-10-21 03:45:25 +0600

    python 02.user_input.py Edited

--- chapter_02/02.user_input.py
+++ chapter_02/02.user_input.py
@@ -4,4 +4,13 @@ print()
 name = input("type your name: ")
 print("hello " + name)
 
+# string
+age = input("How old are you ? ")
+print("Your are " + age + " Years old")
+
+
+
+
+
 print()
+print("Thanks for your information...........")
