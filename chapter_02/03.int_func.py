acdbfa6fb920fb380ab86a81e89fda133a1195f8
TechMainul <nilakashkhan2@gmail.com>
2020-10-21 04:19:40 +0600

    python 03.int_func.py added

---
+++ chapter_02/03.int_func.py
@@ -0,0 +1,23 @@
+# User input
+# Input Function
+
+print("\n checking_01...>>>>>>>>>>>>>>>>>>>>>")
+number_one = int(input("enter your first number: "))
+number_two = int(input("enter your second number: "))
+number_three = int(input("enter your second number: "))
+
+total = number_one + number_two + number_three
+print("total is =" + str(total))
+
+print("\n checking_02...>>>>>>>>>>>>>>>>>>>>> \n")
+number_one = str(4)
+number_two = float(number_two)
+number_three = int(number_three)
+
+print(number_two + number_three) #float print hobe
+
+print(int(number_two + number_three)) #int print hobe
+
+
+print()
+print("Thanks for your checkout.......")
