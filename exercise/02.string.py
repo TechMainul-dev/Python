1a434190a5e74b48c3cad02219a2331abec0aeb5
Khan <techmainul@outlook.com>
2020-10-21 18:56:23 +0600

    some practice python 02.string.py

--- exercise/02.string.py
+++ exercise/02.string.py
@@ -1,5 +1,10 @@
 #Exercise_02
 # Ask user to input 3 numbers and you have to print average of three numbers using string formatting.
 
+x, y, z = int(input("input 3 number like separated x, y, z : "))
+total = x + y + z
+print (str(total))
+
+
 #https://www.youtube.com/watch?v=OxwM4g24Anw&list=PLwgFb6VsUj_lQTpQKDtLXKXElQychT_2j&index=26
 print()
