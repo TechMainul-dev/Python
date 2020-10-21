98c7fcc322968aa4c8cc8e43eb6141923fd67bee
TechMainul <nilakashkhan2@gmail.com>
2020-10-21 22:24:20 +0600

    python 02.string.py complete

--- exercise/02.string.py
+++ exercise/02.string.py
@@ -1,23 +1,8 @@
 #Exercise_02
 # Ask user to input 3 numbers and you have to print average of three numbers using string formatting.
 
-# x, y, z = int(input("input '3' number like separated x, y, z : ").split(","))
+x, y, z = input("input num with separated x, y, z : ").split(",")
 
-x = int(input("x = "))
-y = int(input("x = "))
-z = int(input("x = "))
-print()
+print(f"average = {(int(x) + int(y) + int(z))/3}")
 
-print(f"when, \nx = {x}, y = {y}, z = {z}")
-print(f"so,\nx + y + z = {x} + {y} + {z}")
-total = x + y + z
-print(f"total = {total}")
-average = total //3
-print(f"average = {average}")
-
-
-#total = x + y + z
-#print(str(total))
-
-
-# name, age = input("enter your name,age : ").split(",")
+print(float(5))
