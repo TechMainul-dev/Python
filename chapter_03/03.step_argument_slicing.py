4c8843fac91d0d428fcd38363d3b3e13618a389c
TechMainul <nilakashkhan2@gmail.com>
2020-10-21 23:17:30 +0600

    03.argumetn_s;icing added

---
+++ chapter_03/03.step_argument_slicing.py
@@ -0,0 +1,10 @@
+#slicing / selecting sub sequence 
+# lang = "Python"
+#syntax - [start argument : stop argument -1 : step]
+
+print("Akashkhan"[2:6:2])
+print("Akashkhan"[2::2])
+print("Akashkhan"[::2])
+print("Akashkhan"[6::-1])
+print("Akashkhan"[-1::-1])
+print("Akashkhan"[::-1])  # trick
