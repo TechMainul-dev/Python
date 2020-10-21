e05184bbfb32255d2aeaeef669e194d9095ff782
TechMainul <nilakashkhan2@gmail.com>
2020-10-21 23:07:11 +0600

    02.string_slicing.py added

---
+++ chapter_03/02.string_slicing.py
@@ -0,0 +1,11 @@
+#slicing / selecting sub sequences
+lang = "Python"
+
+#print(lang[4])
+#syntax - [start argument : stop argument]
+
+print(lang[0:7])
+print(lang[-1:7])
+print(lang[:])
+print(lang[3:])
+print(lang[:3])
