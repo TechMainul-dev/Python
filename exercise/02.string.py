e7607306a04419a161912d7ea49f347d602d309d
Khan <techmainul@outlook.com>
2020-10-21 18:08:06 +0600

    test

--- exercise/02.string.py
+++ exercise/02.string.py
@@ -2,5 +2,4 @@
 # Ask user to input 3 numbers and you have to print average of three numbers using string formatting.
 
 #https://www.youtube.com/watch?v=OxwM4g24Anw&list=PLwgFb6VsUj_lQTpQKDtLXKXElQychT_2j&index=26
-
-.
+print()
