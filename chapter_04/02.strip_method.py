04323c14f8adc717786846d5dd85c5e8bb9f0aad
TechMainul <nilakashkhan2@gmail.com>
2020-10-22 21:18:26 +0600

    including .strip method

---
+++ chapter_04/02.strip_method.py
@@ -0,0 +1,7 @@
+name = "    Akash   "
+dots = ".............."
+# lstrip()-method
+print(name + dots)
+print(name.lstrip() + dots)
+print(name.rstrip() + dots)
+print(name.strip() + dots)
--- exercise/04.excercise.py
+++ exercise/04.excercise.py
@@ -8,9 +8,11 @@
 
 name, char = input("Enter your Name, Character with comma separated : ").split(",") 
 
-print(f"Name length : {len(name)}") 
-print(f"Character count when case sensitive: {name.count(char)}") # case sensitive
+print(f"Name length with left/right space: {len(name)}") 
+print(f"Character count when case sensitive: {name.count(char.strip())}") # case sensitive
 
+print()
 # name.lower().count(char.lower())
-
-print(f"Character count when case insensitive: {name.lower().count(char.lower())}") # case insensitive
+# print(name.strip() + dots)
+print(f"Name length without space: {len(name.strip())}") 
+print(f"Character count when case insensitive: {name.lower().count(char.strip().lower())}") # removed (input) left & right spaces
