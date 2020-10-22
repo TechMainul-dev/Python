47c315b1a13257a4ce4f733577f562ca1755ef9f
TechMainul <nilakashkhan2@gmail.com>
2020-10-22 21:19:52 +0600

    02.strip_method.py completly added

--- chapter_04/02.strip_method.py
+++ chapter_04/02.strip_method.py
@@ -1,7 +1,8 @@
-name = "    Akash   "
+name = "    Akash Khan   "
 dots = ".............."
 # lstrip()-method
 print(name + dots)
-print(name.lstrip() + dots)
-print(name.rstrip() + dots)
-print(name.strip() + dots)
+print(name.lstrip() + dots) # remove only left space
+print(name.rstrip() + dots) # remove only right space
+print(name.strip() + dots) # remove left & right space
+print(name.replace(" ","") + dots) # replace all space 
