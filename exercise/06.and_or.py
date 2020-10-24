3c94af6ccb01905bef08b825cd7abc1c17c98c87
TechMainul <nilakashkhan2@gmail.com>
2020-10-25 03:25:48 +0600

    name changes only

---
+++ exercise/06.and_or.py
@@ -0,0 +1,6 @@
+# Exercise - Watch COCO
+# Ask user's name and age
+# if user's name start with ('a' or 'A') and age is above 10 then print "you can watch coco movie"
+# Else print 'sorry', you can't watch coco
+
+name, age = 'Akash', 26
