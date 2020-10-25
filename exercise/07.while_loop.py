4eae827a2a5dd353173868ba19abee4661fa89af
Khan <techmainul@outlook.com>
2020-10-25 15:39:38 +0600

    while_loop exercise added

--- exercise/07.while_loop.py
+++ exercise/07.while_loop.py
@@ -3,3 +3,11 @@
 # ask a user for natural number(n)
 # print total from 1 to n
 
+n = int(input("enter  a natural number : "))
+
+total, i = 0, 1
+
+while i <= n:
+    total += i
+    i += 1
+print(f"sum of 1 to {n} = {total} ")
