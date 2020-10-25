6531307f470132297fba283f18ec3bb9442191c3
Khan <techmainul@outlook.com>
2020-10-25 11:51:15 +0600

    some change see

--- chapter_03/01.if_statement.py
+++
@@ -1,9 +0,0 @@
-# syntax
-# if condition:
-
-#   if condition is true then this code will execute
-
-age = input("enter your age : ")
-age = int(age)
-if age >= 14:
-    print("you are above 14")
--- chapter_03/02.pass.py
+++
@@ -1,6 +0,0 @@
-# Pass statement use for blink result
-
-age = input("enter your age : ")
-age = int(age)
-if age >= 14:
-    pass
