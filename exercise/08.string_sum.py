96c6f1e9516c0d09a66e476ae37778587fd37334
Khan <techmainul@outlook.com>
2020-10-28 16:56:39 +0600

    08.string_sum.py slicing function added

--- exercise/08.string_sum.py
+++ exercise/08.string_sum.py
@@ -1,20 +1,14 @@
 # ask user to input a number containing more than one digit
 # example - 1256
 # calculate 1+2+5+6 and print
+# int(x[i]) +
+
 
 x = input("input a number more than one digit : ")
 
-# int(x[i]) +
-
-total, i, j = 0, 0, 0
-# y = (f"{x[0]} + {x[1]} + {x[2]} + {x[3]}")
-
-y = x[0:1] + "+" + x[1:2]
-
+total, j = 0, 0
+  
 while j < len(x):
-    y = x
-
-while i < len(x):
-    total += int(x[i])
-    i += 1
-print(f"{y} = {total}")
+    total += int(x[j])
+    j += 1
+print(f"{' + '.join(x[i:i + 1] for i in range(0, len(x), 1))} = {total}")
