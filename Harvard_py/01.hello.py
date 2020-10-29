7764ac6b033678f4e9715f637f75463bab35c460
TechMainul <nilakashkhan2@gmail.com>
2020-10-30 00:01:46 +0600

    harvard class exercise added

---
+++ Harvard_py/01.hello.py
@@ -0,0 +1,7 @@
+print("hello word")
+
+a = 28          # int
+b = 1.5         # float
+c = "Hello"     # str
+d = True        # bool
+e = None        # NoneType
---
+++ Harvard_py/02.name.py
@@ -0,0 +1,4 @@
+name = ("World: ")   #input("World: ")
+
+print("Hello, " + name)     # formate - 1
+print(f"Hello, {name}")     # formate - 2
