ff009bdd52646bcb67876e831d09ace91aebd05a
TechMainul <nilakashkhan2@gmail.com>
2020-11-01 18:40:54 +0600

    08.for_loop_sum.py added

---
+++ chapter_03/08.for_loop_sum.py
@@ -0,0 +1,15 @@
+# 1 + 2 + 3 ......... 10
+
+# total = 0
+# for i in range(1,4):
+#     total += i
+
+# print(f" output : {total}")
+
+# ----------------------------------
+
+n = int(input("enter your number : "))
+total = 0
+for i in range(1,n+1):
+    total += 1
+print(total)
