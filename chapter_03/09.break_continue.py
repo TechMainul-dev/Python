581f8f9c08bfd187fda8dda83338d0d0c997b6ba
dell <nilakashkhan@outlook.com>
2020-11-16 09:54:05 +0600

    break & continue added

---
+++ chapter_03/09.break_continue.py
@@ -0,0 +1,16 @@
+# break and continue keyword
+
+# 1 to 10 print
+print("Break keyword\n")
+
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
+print("________________\n")
+print("Continue keyword :")
+
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(i)
