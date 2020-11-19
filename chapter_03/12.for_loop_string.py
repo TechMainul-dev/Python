51e20af1c758607d75f23bcc75194b44a499cd8f
dell <nilakashkhan@outlook.com>
2020-11-19 18:59:47 +0600

    sum function added

--- chapter_03/12.for_loop_string.py
+++ chapter_03/12.for_loop_string.py
@@ -1,7 +1,14 @@
-name = "Akash"
-
+# name = "Akash"
 # for i in range(len(name)):
 #     print(name[i])
 
-for i in 'khan':
-    print(i)
+# for i in 'khan':
+#     print(i)
+
+# 1256   ---->1+2+5+6
+
+num = input("enter a number :")
+total = 0
+for i in num:
+    total += int(i)
+print(total)
