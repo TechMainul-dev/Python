b13635490cf935d96aad8b173d6e42798d40335f
dell <nilakashkhan@outlook.com>
2020-11-19 17:57:10 +0600

    DRY - don't repeat yourself

---
+++ chapter_03/10.dry.py
@@ -0,0 +1,18 @@
+# break and continue keyword
+
+# 1 to 10 print
+print("Break keyword\n")
+
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
+print("________________\n")
+print("Continue keyword :")
+
+for i in range(1, 11):
+    if i == 9:
+        continue
+    print(i)
+
+# DRY - don't repeat yourself
