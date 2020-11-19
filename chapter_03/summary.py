9c1eae14ec735eae94aed1c422a0f962f7e37aa8
dell <nilakashkhan@outlook.com>
2020-11-19 20:00:06 +0600

    basic changes only

--- chapter_03/summary.py
+++ chapter_03/summary.py
@@ -1,4 +1,4 @@
-# if statement
+print('\n______if statement______\n')
 name = "khan"
 if name == "Akash" or name == "akash":
     print("You are Akash")
@@ -6,9 +6,11 @@ elif name == "khan" or name == "Khan":
     print("Your are khan")
 else:
     print("You are not Akash or Khan")
-
-# While loop
+# ====================================
+print('\n______While loop______\n')
 i = 1
 while i <= 10:
-    print(f"{i} Khan")
+    print(f"{i} {name}")
     i += 1
+# ====================================
+print('\n______for loop______\n')
