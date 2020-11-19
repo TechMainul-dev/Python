b15e1af74b975497874258593261458ee83f9bbc
dell <nilakashkhan@outlook.com>
2020-11-19 19:47:24 +0600

    while loop basic

--- chapter_03/summary.py
+++ chapter_03/summary.py
@@ -6,3 +6,9 @@ elif name == "khan" or name == "Khan":
     print("Your are khan")
 else:
     print("You are not Akash or Khan")
+
+# While loop
+i = 0
+while i < 10:
+    print("hello world")
+    i += 1
