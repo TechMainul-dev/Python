e3816fd9049efaa32e8710f6e892448fe8fab1d4
dell <nilakashkhan@outlook.com>
2020-11-19 18:14:52 +0600

    break use another way

--- chapter_03/10.dry.py
+++ chapter_03/10.dry.py
@@ -7,10 +7,10 @@ guess = 1
 number = int(input("guess a number between 1 and 100 : "))
 game_over = False
 
-while not game_over:
+while True:
     if number == winning_number:
         print(f"you win, and you guessed this number in {guess} times ")
-        game_over = True
+        break
     else:
         if number < winning_number:
             print("too low ")
