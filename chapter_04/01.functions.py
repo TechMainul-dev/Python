988eb9826fbf658680b671dd26f28734e387d992
dell <nilakashkhan@outlook.com>
2020-11-22 18:44:27 +0600

    introduction to functions

--- chapter_04/01.functions.py
+++ chapter_04/01.functions.py
@@ -9,6 +9,10 @@ def add_two(num1, num2):
     return num1 + num2
 
 
-a = int(input("enter first number : "))
-b = int(input("enter second number : "))
-print(add_two(a, b))
+# a = int(input("enter first number : "))
+# b = int(input("enter second number : "))
+# print(add_two(a, b))
+
+first_name = input("type yout first name :")
+last_name = input("type yout last name :")
+print(add_two(first_name, last_name))
