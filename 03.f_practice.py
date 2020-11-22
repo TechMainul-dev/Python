488991be6341e45abde53e494ef1a5ca1d71db8a
dell <nilakashkhan@outlook.com>
2020-11-22 19:28:38 +0600

    boolient f added

--- 03.f_practice.py
+++ 03.f_practice.py
@@ -17,3 +17,13 @@ def odd_even(num):
 
 
 print(odd_even(11))
+print('--------------------------------')
+
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+
+print(is_even(10))
