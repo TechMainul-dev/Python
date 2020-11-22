c217cedc3befea6ef7a1e782a2b34372d5ace4dd
dell <nilakashkhan@outlook.com>
2020-11-22 18:52:49 +0600

    print vs return deffrence

--- 02.return_print_f.py
+++ 02.return_print_f.py
@@ -3,8 +3,7 @@ print('----------------------------------')
 
 
 def add_three(a, b, c):
-    print(a + b + c)
+    return a + b + c
 
 
-# print(add_three(5, 5, 5))
-add_three(5, 5, 5)
+print(add_three(5, 5, 5))
