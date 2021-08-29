a95bd1dc0af9cf25c17a8f2730d8919c3a10248e
dell <nilakashkhan@outlook.com>
2021-08-29 17:49:40 +0600

    up

---
+++ exercise/practise.py
@@ -0,0 +1,24 @@
+# name, age = "Akash", 27
+# print("Your Name is : " + name + " \n your age is : " + str(age))
+# ___________________________________________________________________
+
+# name, age = input("enter your name & age: ").split(",")
+# print(name)
+# print(age)
+# ___________________________________________________________________
+
+# print("Hello {}, your age is {}".format(name, age)) this is for python 3.0
+# print(f"Hello {name}, your age is {age}") #this is for python 3.0
+# ___________________________________________________________________
+
+# num1, num2, num3 = 2, 3, 4
+# total = num1 + num2 + num3
+# print(f"total is : {total}")
+# print(f"Average number is :{total/3}")
+# ___________________________________________________________________
+
+# num1, num2, num3 = input("Enter three number separated by comma :").split(",")
+# print(f"Total number is :{(int(num1) + int(num2) + int(num3))} \n& Average number is : {(int(num1) + int(num2) + int(num3)) / 3}")
+# ___________________________________________________________________
+
+String Indexing: Python tutorial 25
