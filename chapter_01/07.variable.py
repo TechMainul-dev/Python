ccdb741f4ac5cb93c03e19df94180d9825cab55a
dell <nilakashkhan@outlook.com>
2021-08-29 17:48:36 +0600

    practice file updated from ofc

--- chapter_01/07.variable.py
+++ chapter_01/07.variable.py
@@ -1,13 +1,13 @@
-# number1=2
-# print(number1)
-# number1=4
-# print(number1)
+number1 = 2
+print(number1)
+number1 = 4
+print(number1)
 
-# name="Mainul Hasan Khan"
-# print(name)
-# name=123
-# print(name)
+_0name = "Mainul Hasan Khan"
+print(_0name)
+name = 123
+print(name)
 
 print("Letter first: test01")
-print('snake case writing: Tech_Mainul') #python 
-print('camel case writing: TechMainul') #Java
+print('snake case writing: Tech_Mainul')  # python
+print('camel case writing: TechMainul')  # Java
