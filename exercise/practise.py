0d94974e46731018d20453a2dfd9d20f58686ba4
dell <nilakashkhan@outlook.com>
2021-09-02 10:51:45 +0600

    practice

--- exercise/practise.py
+++ exercise/practise.py
@@ -17,8 +17,54 @@
 # print(f"Average number is :{total/3}")
 # ___________________________________________________________________
 
-# num1, num2, num3 = input("Enter three number separated by comma :").split(",")
+# num1, num2, num3 = 1,2,3
+# # num1, num2, num3 = input("Enter three number separated by comma :").split(",")
+
 # print(f"Total number is :{(int(num1) + int(num2) + int(num3))} \n& Average number is : {(int(num1) + int(num2) + int(num3)) / 3}")
 # ___________________________________________________________________
 
-String Indexing: Python tutorial 25
+# name = "Akash Khan"
+# print(name[-7])
+# print(name[7])
+
+# --------String slicing
+# print(name[2:8])
+# print(name[2:])
+# print(name[:8])
+
+# --------step argument
+#print("Akash khan"[0:8])
+# print("Akash khan"[0:8:2])  # --third letter skip steps
+# print("Akash khan"[3::-1])  # --backword
+
+# ---Exercise
+# name = input("Enter your name :")
+# print(f"reverse of your name is :{name[::-1]}")
+# _____________________________________________________________
+
+# # --String method part-1
+
+# # len function
+# print(len(name))
+# print(len("Akash khan"))
+
+# # lower method
+# print(name.lower())
+
+# # Upper method
+# print(name.upper())
+
+# # title method
+# print(name.title())
+
+# # Count method
+# print(name.count("a"))
+# ----------------------------------------------------------------
+# name, char = "Akash khAn", "A"
+# # name, char = input("Enter comma separeted Name & Character :").split(",")
+
+
+# print(f"Lenth of your name :{len(name.lower())}")
+# print(f"Character count :{name.lower().count(char.strip().lower())}")
+
+#--Replace & find method
